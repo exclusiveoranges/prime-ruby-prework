0cbe974d8179e2f95c8f7cc63f218b61bee26e4b
Ryan O'Toole <otoolerj@gmail.com>
2018-03-22 02:05:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 def prime?(n)
   (2..(n-1)).each do |d|
     if n =< 1
-      return false 
-    else (n % d) == 0
       return false
-     1 > n
+    else (n % d) == 0
       return false
     end
   end
